005467bce41316cc29a377bc040a74c4f6eaba5b
Dillon Lloyd Lee & Joe Hartman <student@epicodus.com>
2015-08-13 10:00:14 -0700

    pass first test: single, simple letter.

--- src/ValueFinder.php
+++ src/ValueFinder.php
@@ -3,13 +3,18 @@
 
         function findWordTotal($word_input) {
 
+            $num_output = 0;
 
+            $word_array = array($word_input);
 
-            
+            foreach ($word_array as $letter) {
+                $num_output = $num_output + 1;
+            }
+
+            $str_output = strval($num_output);
+
+            return $num_output;
+
         }
     }
-
-
-
-
 ?>
